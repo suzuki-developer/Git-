8ec350b758356992c77fda74278159b139b2be96
001-0076\suzuki-s <suzuki-s@ferix.jp>
2023-06-08 11:34:11 +0900

    Flask_dev06が正常な地点に戻ってからやり直すため、新規作成したFlask_07,08を退避

--- Flask_dev07/app/forms/auth_form.py
+++
@@ -1,7 +0,0 @@
-from flask_wtf import Form
-from wtforms import TextField, PasswordField
-from wtforms.validators import Required, Email
-
-class LoginForm(Form):
-    email    = TextField('Email Adress', [Email(), Required(message='Forgot your email address?')])
-    password = PasswordField('Password', [Required(message='Must provide a password.')])
